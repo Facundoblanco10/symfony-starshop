277f876127baacdf5a2fd38885557ca053e97566
Facundo <fblanco@oneplay.net>
2025-03-20 15:05:17 -0300

    Initial commit for Symfony Starshop

---
+++ src/Controller/MainController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController
+{
+
+    #[Route('/')]
+    public function homepage()
+    {
+        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
+    }
+
+    #[Route('/about')]
+    public function about()
+    {
+        return new Response('Starshop is a new online store for Starship parts. We are here to help you find the parts you need to keep your Starship flying!');
+    }
+}
